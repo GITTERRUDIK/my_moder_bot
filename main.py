073cf2d878bd88f4ebdade0545872413cedba005
GITTERRUDIK <kyrica777vitya@gmail.com>
2024-12-20 12:11:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 
 # Задайте префикс для команд
+intents = discord.Intents.default()
+intents.message_content = True
 bot = commands.Bot(command_prefix="!")
 
 # Событие: бот готов к работе
